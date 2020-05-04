b526ad0cb6d4417463843b4abf8b604683e5472c
Maël Querré <mael.querre@gmail.com>
2020-05-04 21:33:16 +0200

    Update controls and format code

--- src/app.js
+++ src/app.js
@@ -15,19 +15,18 @@ function adjustHeight() {
 
 //** FOLLOWING CODE IS DEDICATED TO THE DEMO **//
 
-window.addEventListener( 'DOMContentLoaded', demo )
+window.addEventListener('DOMContentLoaded', demo)
 
 function demo() {
-
     const
-        WIDTH = document.getElementById( 'width' ),
-        HEIGHT = document.getElementById( 'height' ),
-        HANDLEABLE = document.querySelector( '.fit-this-text' ),
-        RESET = document.getElementById( 'reset' ),
+        WIDTH = document.getElementById('width'),
+        HEIGHT = document.getElementById('height'),
+        HANDLEABLE = document.querySelector('.fit-this-text'),
+        RESET = document.getElementById('reset'),
         INITIAL_VALUES = [WIDTH.value, HEIGHT.value],
-        WIDEABLE = document.getElementById( 'wideable' ),
-        WIDTH_OUTPUT = document.getElementById( 'widthVal' ),
-        HEIGHT_OUTPUT = document.getElementById( 'heightVal' )
+        WIDEABLE = document.getElementById('wideable'),
+        WIDTH_OUTPUT = document.getElementById('widthVal'),
+        HEIGHT_OUTPUT = document.getElementById('heightVal')
 
     WIDTH.oninput = handleWidth
     HEIGHT.oninput = handleHeight
@@ -36,18 +35,18 @@ function demo() {
 
 
     function handleWidth() {
-        WIDTH_OUTPUT.value = HANDLEABLE.style.width = `${this.value}%`
+        WIDTH_OUTPUT.value = HANDLEABLE.style.width = this.value
         fit([HANDLEABLE], WIDEABLE.checked)
     }
 
     function handleHeight() {
-        HEIGHT_OUTPUT.value = HANDLEABLE.style.height = `${this.value}%`
+        HEIGHT_OUTPUT.value = HANDLEABLE.style.height = this.value
         fit([HANDLEABLE], WIDEABLE.checked)
     }
 
     function reset() {
-        WIDTH_OUTPUT.value = HANDLEABLE.style.width = `${INITIAL_VALUES[0]}%`
-        HEIGHT_OUTPUT.value = HANDLEABLE.style.height = `${INITIAL_VALUES[1]}%`
+        WIDTH_OUTPUT.value = HANDLEABLE.style.width = INITIAL_VALUES[0]
+        HEIGHT_OUTPUT.value = HANDLEABLE.style.height = INITIAL_VALUES[1]
         WIDTH.value = INITIAL_VALUES[0]
         HEIGHT.value = INITIAL_VALUES[1]
         fit([HANDLEABLE], WIDEABLE.checked)
