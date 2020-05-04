a01e23469a647e36ff1940215e610e313229cc64
Maël Querré <mael.querre@gmail.com>
2020-05-04 19:12:57 +0200

    Format code so it follows the vast majority of formatting conventions

--- src/fitext.js
+++ src/fitext.js
@@ -1,48 +1,52 @@
-export default function fit( fittables, wideable ) {
-    [...fittables].forEach( box => {
+export default function fit(fittables, wideable) {
+    [...fittables].forEach(box => {
 
         const WRAPPER_CLASSNAME = 'fitter'
 
-        if ( !RegExp( WRAPPER_CLASSNAME ).test( box.firstElementChild.className ) )
+        if (!RegExp(WRAPPER_CLASSNAME).test(box.firstElementChild.className)) {
             box.innerHTML = `<div class='${WRAPPER_CLASSNAME}'>${box.innerHTML}</div>`
+        }
 
-        const
-            FITTER = box.firstElementChild,
-            CHILDREN = [...box.getElementsByTagName( '*' )]
+        const fitter = box.firstElementChild
+        const children = [...box.getElementsByTagName('*')]
 
-        FITTER.style.display = 'inline-block'
+        fitter.style.display = 'inline-block'
 
         const core = () => {
-            const
-                OVERFLOWING = () => FITTER.offsetHeight > parseFloat( getComputedStyle( box ).height ),
-                UPDATE_FONT_SIZE = ( child, multiplier ) => child.style.fontSize = `${parseFloat( getComputedStyle( child ).fontSize ) * multiplier}px`
+            const overflowing = () => fitter.offsetHeight > parseFloat(getComputedStyle(box).height)
+            const updateFontSize = (child, multiplier) => child.style.fontSize = `${parseFloat(getComputedStyle(child).fontSize) * multiplier}px`
 
-
-            CHILDREN.forEach( child => {
-                if ( !child.dataset.size )
-                    child.dataset.size = getComputedStyle( child ).fontSize
-            } )
+            children.forEach(child => {
+                if (!child.dataset.size) {
+                    child.dataset.size = getComputedStyle(child).fontSize
+                }
+            })
 
             function check() {
-                if ( OVERFLOWING() )
-                    while ( OVERFLOWING() )
-                        CHILDREN.forEach( child => UPDATE_FONT_SIZE( child, .99 ) )
-                else
-                    if ( wideable )
-                        while ( !OVERFLOWING() )
-                            CHILDREN.forEach( child => UPDATE_FONT_SIZE( child, 1.01 ) )
-                    else
-                        CHILDREN.forEach( child => parseFloat( child.style.fontSize ) * 1.01 < child.dataset.size
-                            ? UPDATE_FONT_SIZE( child, 1.01 )
-                            : child.style.removeProperty( 'font-size' ) )
+                if (overflowing()) {
+                    while (overflowing())
+                        children.forEach(child => updateFontSize(child, .99))
+                } else {
+                    if (wideable) {
+                        while (!overflowing())
+                            children.forEach(child => updateFontSize(child, 1.01))
+                    } else {
+                        children.forEach(child => parseFloat(child.style.fontSize) * 1.01 < child.dataset.size
+                            ? updateFontSize(child, 1.01)
+                            : child.style.removeProperty('font-size'))
+                    }
+                }
 
-                if ( OVERFLOWING() )
+                if (overflowing()) {
                     check()
+                }
             }
+
             check()
         }
+
         core()
 
-        FITTER.style.removeProperty( 'display' )
-    } )
+        fitter.style.removeProperty('display')
+    })
 }
