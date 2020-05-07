81107a4010597d8e2b2709d7f9dd7f83e3c942e4
Maël Querré <mael.querre@gmail.com>
2020-05-07 23:18:50 +0200

    Add padding input

--- src/app.js
+++ src/app.js
@@ -15,15 +15,22 @@ window.addEventListener('DOMContentLoaded', () => {
 //** FOLLOWING CODE IS DEDICATED TO THE DEMO **//
 
 function demo() {
+    // Form inputs
     const widthInput = document.querySelector('#widthInput')
     const heightInput = document.querySelector('#heightInput')
+    const paddingInput = document.querySelector('#paddingInput')
+
+    // Form outputs
     const widthOutput = document.querySelector('#widthOutput')
     const heightOutput = document.querySelector('#heightOutput')
+    const paddingOutput = document.querySelector('#paddingOutput')
+
     const resetButton = document.querySelector('#resetButton')
 
     const enlargeCheckbox = document.querySelector('#enlargeCheckbox')
 
     const initialSize = [widthInput.value, heightInput.value]
+    const initialPadding = paddingInput.value
     const fitterContainer = document.querySelector('.fitter-container')
 
     function handleWidth({ target }) {
@@ -38,9 +45,16 @@ function demo() {
         fit([fitterContainer], enlargeCheckbox.checked)
     }
 
+    function handlePadding({ target }) {
+        paddingOutput.value = target.value
+        fitterContainer.style.padding = `${target.value}px`
+        fit([fitterContainer], enlargeCheckbox.checked)
+    }
+
     function reset() {
         widthInput.value = initialSize[0]
         heightInput.value = initialSize[1]
+        paddingInput.value = initialPadding
 
         widthOutput.value = initialSize[0]
         heightOutput.value = initialSize[1]
@@ -57,11 +71,13 @@ function demo() {
 
     widthInput.oninput = handleWidth
     heightInput.oninput = handleHeight
+    paddingInput.oninput = handlePadding
     resetButton.onclick = reset
     enlargeCheckbox.onchange = handleWideable
 
     // Init
     fitterContainer.style.width = `${widthInput.value}%`
     fitterContainer.style.height = `${heightInput.value}%`
+    fitterContainer.style.padding = `${paddingInput.value}px`
     fit([fitterContainer], enlargeCheckbox.checked)
 }
